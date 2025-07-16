638879b00aa884578d2d9b66249484f603492286
Flipsales <flipsales@outlook.com>
2025-07-16 09:01:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['placeholder.svg'],
-    unoptimized: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    unoptimized: true,
+  },
 }
-export default nextConfig
+
+module.exports = nextConfig
